a99197af57186053de546c0c94843d176694e479
Dan Rubins <dan.rubins@agnitogroup.com>
2014-12-07 20:20:00 -0800

    Added export for Raphael

--- package.js
+++ package.js
@@ -1,12 +1,14 @@
 Package.describe({
-  summary: "Raphael.js - JavaScript Vector Library",
-  version: "0.0.2",
+  summary: "Raphael - JavaScript Vector Library",
+  version: "0.1.0",
   git: "https://github.com/Agnito/meteor-raphael.git",
   name: "agnito:raphael"
 });
 
 Package.onUse(function (api, where) {
   api.versionsFrom("1.0");
+  api.use('jquery');
   api.addFiles('raphael.js', 'client');
+  api.export('Raphael', 'client');
 });
 
